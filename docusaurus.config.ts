431e8913451b74fc7266a7ce7070c7e15d26aa12
MoisesAzevedo <MoisesDev2022@gmail.com>
2026-08-27 10:47:33 -0300

    ajustes na ui

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -1,11 +1,11 @@
-import {themes as prismThemes} from 'prism-react-renderer';
-import type {Config} from '@docusaurus/types';
+import { themes as prismThemes } from 'prism-react-renderer';
+import type { Config } from '@docusaurus/types';
 import type * as Preset from '@docusaurus/preset-classic';
 
 // This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
 
 const config: Config = {
-  title: 'My Site',
+  title: 'Mercapecas - Backend',
   tagline: 'Dinosaurs are cool',
   favicon: 'img/favicon.ico',
 
@@ -75,9 +75,9 @@ const config: Config = {
       respectPrefersColorScheme: true,
     },
     navbar: {
-      title: 'My Site',
+      title: 'Mercapecas - Backend',
       logo: {
-        alt: 'My Site Logo',
+        alt: 'Mercapecas - Backend Logo',
         src: 'img/logo.svg',
       },
       items: [
@@ -85,12 +85,12 @@ const config: Config = {
           type: 'docSidebar',
           sidebarId: 'tutorialSidebar',
           position: 'left',
-          label: 'Tutorial',
+          label: 'Documentação',
         },
-        {to: '/blog', label: 'Blog', position: 'left'},
+        { to: '/blog', label: 'Blog', position: 'left' },
         {
-          href: 'https://github.com/facebook/docusaurus',
-          label: 'GitHub',
+          href: 'https://github.com/mercapecas-casa-do-mercedes/site-backend',
+          label: 'Repositório',
           position: 'right',
         },
       ],
@@ -138,7 +138,7 @@ const config: Config = {
           ],
         },
       ],
-      copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+      copyright: `Copyright © ${new Date().getFullYear()} Mercapecas - Backend. Built with Docusaurus.`,
     },
     prism: {
       theme: prismThemes.github,
